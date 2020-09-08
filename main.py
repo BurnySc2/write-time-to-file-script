e6137dc105b0e69d99609931f6c93aa905f3b48b
BuRny <gamingburny@gmail.com>
2020-09-09 01:36:10 +0200

    Make config.json variable, give error messages if file could not be found, make time_format string variable through config.json

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import time
 import datetime
+import json
+import os
 
 file_path = "time.txt"
+config_path = "config.json"
 
 
 def main():
+    time_format = "%H:%M:%S"
+    if os.path.exists(config_path):
+        with open("config.json") as f:
+            j = json.load(f)
+            time_format = j.get("time_format", "%H:%M:%S")
+            print("Loaded {} - set time format to: '{}'".format(config_path, time_format))
+    else:
+        print("{} file could not be found. Setting default format to '{}'".format(config_path, time_format))
+        # Write default config file
+        with open(config_path, "w") as f:
+            json.dump({"time_format": time_format}, f)
+
     formatted_time = ""
     while 1:
         my_time = datetime.datetime.now()
-        # Remove microseconds part
-        my_time = my_time - datetime.timedelta(microseconds=my_time.microsecond)
         # print(my_time)
 
         # Format time https://www.w3schools.com/python/python_datetime.asp
-        time_string = my_time.strftime("%H:%M:%S")
+        time_string = my_time.strftime(time_format)
         if formatted_time != time_string:
             formatted_time = time_string
             with open(file_path, "w") as f:
